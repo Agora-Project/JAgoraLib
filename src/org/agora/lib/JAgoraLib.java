a951bdb0f66ce724e0d81840e922545444030de7
João G. Martins <anvilfolk@gmail.com>
2013-10-23 23:31:09 -0400

    Moved JAgoraLib over from JAgoraServer, added login/logout comms.

---
+++ src/org/agora/lib/JAgoraLib.java
@@ -0,0 +1,174 @@
+package org.agora.lib;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.agora.logging.Log;
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+
+public class JAgoraLib {  
+  
+  protected String sessionID;
+  protected String hostname;
+  protected int port;
+
+  public JAgoraLib() {
+    sessionID = null;
+  }
+  
+  
+  
+  private Socket openConnection() {
+    return openConnection(hostname, port);
+  }
+  
+  private Socket openConnection(String hostname, int port) {
+    Socket s = null;
+    try {
+      s = new Socket(hostname, port);
+    } catch (UnknownHostException e) {
+      Log.error("[JAgoraLib] Could not identify host.");
+      Log.error(e.getMessage());
+    } catch (IOException e) {
+      Log.error("[JAgoraLib] Error opening connection to " + hostname + ":"+port);
+      Log.error(e.getMessage());
+    }
+    return s;
+  }
+  
+  private boolean closeConnection(Socket s) {
+    try {
+      s.close();
+      return true;
+    } catch (IOException e) {
+      Log.error("[JAgoraLib] Could not close connection " + s);
+    }
+    return false;
+  }
+  
+  /**
+   * Constructs an Agora protocol login request in BSON.
+   * @param user
+   * @param password
+   * @return
+   */
+  private BSONObject constructLoginRequest(String user, String password) {
+    BSONObject bson = new BasicBSONObject();
+    bson.put("action", JAgoraComms.LOGIN_ACTION);
+    bson.put("user", user);
+    bson.put("pass", password);
+    return bson;
+  }
+  
+  /**
+   * Tries to parse an Agora login response from BSON. Stores a session ID
+   * that should be used in all further communications.
+   * @param bson
+   * @return
+   */
+  private boolean parseLoginResponse(BSONObject bson) {
+    boolean success = (boolean) bson.get("success");
+    if (!success)
+      return false;
+    // Success!
+    sessionID = (String)bson.get("sessionID");
+    return sessionID != null;
+  }
+  
+  /**
+   * Performs a login with an Agora server.
+   * @param hostname The Agora server.
+   * @param port The port on which the server is listening.
+   * @param user
+   * @param password
+   * @return
+   */
+  public boolean connect(String hostname, int port, String user, String password) {
+    // TODO: Can't differentiate between what happened. Make return int?
+    Socket s = openConnection(hostname, port);
+    boolean success = JAgoraComms.writeBSONObjectToSocket(s,
+        constructLoginRequest(user, password));
+    if (!success) {
+      Log.error("[JAgoraLib] Could not send login message.");
+      return false;
+    }
+
+    BSONObject response = JAgoraComms.readBSONObjectFromSocket(s);
+    if (response == null) {
+      Log.error("[JAgoraLib] Could not read login response.");
+      return false;
+    }
+    success = parseLoginResponse(response);
+    if (!success) {
+      Log.error("[JAgoraLib] Wrong login information.");
+      return false;
+    }
+
+    success = closeConnection(s);
+    if (!success) {
+      Log.error("[JAgoraLib] Problems closing login connection.");
+      return false;
+    }
+    Log.debug("[JAgoraLib] Successful login for " + user);
+    return true;
+  }
+  
+  
+  
+  
+  private BSONObject constructLogoffRequest() {
+    BSONObject bson = new BasicBSONObject();
+    bson.put("action", JAgoraComms.LOGOUT_ACTION);
+    bson.put("session", sessionID);
+    return bson;
+  }
+  
+  private boolean parseLogoffResponse(BSONObject bson) {
+    return (int)bson.get("response") == JAgoraComms.SERVER_OK;
+  }
+  
+  /**
+   * Closes the current Agora session.
+   * @return
+   */
+  public boolean close() {
+    if (!isConnected()) {
+      return false;
+    }
+    
+    Socket s = openConnection();
+    if (s != null) {
+      Log.error("... in JAgoraLib.parseLogoffResponse.");
+      return false;
+    }
+    
+    boolean success = JAgoraComms.writeBSONObjectToSocket(s, constructLogoffRequest());
+    if (!success) {
+      Log.error("... in JAgoraLib.close().");
+      return false;
+    }
+    
+    BSONObject response = JAgoraComms.readBSONObjectFromSocket(s);
+    if (response != null) {
+      Log.error("... in JAgoraLib.close().");
+      return false;
+    }
+    
+    success = parseLogoffResponse(response);
+    if(!success){
+      Log.error("... in JAgoraLib.close().");
+      return false;
+    }
+    
+    sessionID = null;
+    hostname = null;
+    return true;
+  }
+  
+  
+  public boolean isConnected() {
+    return sessionID != null;
+  }
+}
---
+++ src/org/agora/lib/Options.java
@@ -0,0 +1,12 @@
+package org.agora.lib;
+
+public class Options {
+  
+  // Network options
+  public static int AGORA_PORT = 1597;
+  
+  // Logging options
+  public static boolean LOG_MESSAGES = true;
+  public static boolean DEBUG_MESSAGES = true;
+  public static boolean ERROR_MESSAGES = true;
+}
