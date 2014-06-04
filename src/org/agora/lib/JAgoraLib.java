88bf8bf5931dcea66eb75e1945ca1254779e51e6
João Martins <jmartins@cs.cmu.edu>
2014-06-04 11:56:19 -0400

    Fixed eclipse class path. Fixed warnings.

--- src/org/agora/lib/JAgoraLib.java
+++ src/org/agora/lib/JAgoraLib.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import org.agora.graph.JAgoraGraph;
 import org.agora.graph.JAgoraNodeID;
 import org.agora.graph.JAgoraThread;
-import static org.agora.lib.IJAgoraLib.ACTION_FIELD;
-import static org.agora.lib.IJAgoraLib.ARGUMENT_ID_FIELD;
 import org.agora.logging.Log;
 import org.bson.BasicBSONObject;
 
